a5c2fe251d8251f0c891ddc497127d8ede6abf4d
Nephew20 <cofieldk20@gmail.com>
2023-07-05 19:26:26 -0400

    fixed server connectivity issues

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,12 +1,4 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+module.exports = { typeDefs, resolvers }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,40 +1,35 @@
 const jwt = require('jsonwebtoken');
 
-// Set token secret and expiration date
+// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // Middleware function for authentication
-  authMiddleware: function (req, res, next) {
-    let token = req.headers.authorization || '';
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (token.startsWith('Bearer ')) {
-      // Remove 'Bearer ' from the token
-      token = token.substring(7, token.length);
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      // If no token is present, throw an error
-      return res.status(401).json({ message: 'You have no token!' });
+      return req;
     }
 
     try {
-      // Verify the token and extract the user data
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-      next();
-    } catch (err) {
+    } catch {
       console.log('Invalid token');
-      return res.status(401).json({ message: 'Invalid token!' });
     }
-  },
 
-  // Function to sign a JWT token
+    return req;
+  },
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    // Create a new JWT token with the payload, secret, and expiration time
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
